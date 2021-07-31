b69b8211ae54737ae90ef6fa4ebe88c815a43cc9
sisi397 <74189924+sisi397@users.noreply.github.com>
2021-08-01 00:29:50 +0900

    08.01 아이디 찾기, 비밀번호 찾기 추가

---
+++ src/main/java/com/codewith/codewith/controller/UserCodeController.java
@@ -0,0 +1,50 @@
+package com.codewith.codewith.controller;
+
+import com.codewith.codewith.model.UserCode;
+import com.codewith.codewith.model.UserInfo;
+import com.codewith.codewith.repository.UserCodeRepository;
+import com.codewith.codewith.dto.UserCodeRequestDto;
+import com.codewith.codewith.service.UserCodeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.security.Principal;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class UserCodeController {
+
+    private final UserCodeRepository userCodeRepository;
+    private final UserCodeService userCodeService;
+    @Resource
+    private UserInfo userInfo;
+
+    @GetMapping("/api/userCode")
+    public List<UserCode> getUserCode() {
+        String userId = userInfo.getUserId();
+        return userCodeRepository.findAllByUserId(userId).orElseThrow(
+                () -> new IllegalArgumentException("userId가 존재하지 않습니다."));
+    }
+
+    //POST (INSERT)
+    @PostMapping("/api/userCode")
+    public UserCode createUserCode(@RequestBody UserCodeRequestDto requestDto) {
+        UserCode userCode = new UserCode(requestDto);
+        return userCodeRepository.save(userCode);
+    }
+
+    //PUT (UPDATE)
+    @PutMapping("/api/userCode/{id}")
+    public Long updateUserCode(@PathVariable Long id, @RequestBody UserCodeRequestDto requestDto) {
+        return userCodeService.update(id, requestDto);
+    }
+
+    //DELETE
+    @DeleteMapping("/api/userCode/{id}")
+    public Long deleteUserCode(@PathVariable Long id){
+        userCodeRepository.deleteById(id);
+        return id;
+    }
+}
